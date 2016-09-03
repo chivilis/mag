4fa8e6adc3254becac9c4e3167122752999a4f35
diugonis <mariusxcivilis@gmail.com>
2016-09-03 12:38:58 +0300

    this is message

---
+++ old_files/ButtonsHandler.js
@@ -0,0 +1,16 @@
+module.exports = {
+	showPrintButtons: function(buttons) {
+		this.b = new Array(buttons);
+		
+		this.b.forEach(function(button) {
+			button.show();
+		});
+	},
+	hidePrintButtons: function(buttons) {
+		this.b = new Array(buttons);
+		
+		this.b.forEach(function(button) {
+			button.hide();
+		});
+	}
+}

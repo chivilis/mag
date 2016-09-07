0892b685087e4d7ab5eed540eee733f2f1769826
diugonis <mariusxcivilis@gmail.com>
2016-09-07 16:56:41 +0300

    printing with dialogue

--- main.js
+++ main.js
@@ -1,101 +1,53 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const {ipcMain, dialog} = require('electron');
+const fs = require('fs');
 
-const {ipcMain} = require('electron')
-
-
-ipcMain.on('synchronous-message', (event, arg) => {
-  console.log(arg)  // prints "ping"
-  event.returnValue = 'pong'
-})
-/*
-	ipc.on('asynchronous-message', function(event, arg) {
-	  console.log(arg)  // prints "ping"
-	  event.sender.send('asynchronous-reply', 'pong')
-	})
-
-	ipc.on('synchronous-message', function(event, arg) {
-	  console.log(arg)  // prints "ping"
-	  event.returnValue = 'pong'
-	})
-*/
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-var mainWindow
-
-//function createWindow () {
-  // Create the browser window.
+var mainWindow = null;
 
 app.commandLine.appendSwitch('enable-usermedia-screen-capturing');
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 
 app.on('ready', function() {
+	
 	mainWindow = new BrowserWindow({width: 570, height: 800/*, frame: false*/,
 								 movable: true,
 								 resizable: true});
-  mainWindow.setMenu(null);
+	mainWindow.setMenu(null);
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+	mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-  mainWindow.webContents.openDevTools()
-  //console.log(mainWindow.webContents)
-  //mainWindow.webContents.print()
+	mainWindow.webContents.openDevTools();
+  
+	ipcMain.on('print', function(event, data) {
+		mainWindow.webContents.print(function() {
+			event.sender.send('printDone');
+		});
+	});
 
-	//ipc.on('print', function() {
-		///console.log(arg)  // prints "ping"
-		/*
-		if(arg == '1') {
-			event.sender.send('asynchronous-reply', 'arg is 1')
-			mainWindow.webContents.print();
-		}
-		else {
-			event.sender.send('asynchronous-reply', 'arg is ' + arg);
-		}
-		*/
-		//console.log(mainWindow.webContents)
-		ipcMain.on('print', (event, arg) => {
-		  console.log(arg)  // prints "ping"
-			console.log('got by side')
-			mainWindow.webContents.print();
-
-		})
-	//	console.log(arg + ' received')
-//});
+	ipcMain.on('printPdf', function(event, data) {
+		mainWindow.webContents.printToPDF({landscape: false }, function(err, data) {
+			dialog.showSaveDialog({title: 'Save PDF file', filters: [{name: ' ', extensions: ['pdf']}] },function(fileName) {
+				fs.writeFile(fileName, data, function(err) {
+					event.sender.send('printDone');
+				});
+			});
+		});
+	});
+		
 	mainWindow.on('closed', function () {
-
-	})
+		;
+	});
 });
-  // Emitted when the window is closed.
-
-
-
-
-
-
-//let contents = mainWindow.webContents
-
-// Quit when all windows are closed.
+ 
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    //createWindow()
+	;
   }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+});
